1fdbac725e20b781b0743610609521bb4cc701b6
Bar Malka <banditsystem@gmail.com>
2017-08-10 17:27:16 +0300

    update

---
+++ core/server.js
@@ -0,0 +1,64 @@
+var os = require('os');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var fs = require("fs-extra");
+var cors = require('cors');
+var connect = require("connect");
+var morgan = require('morgan')
+
+morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+  ].join(' ')
+})
+
+var app = express();
+var http = require('http').Server(app);
+
+app.use(morgan('combined'))
+app.use(cors({origin:true,credentials:true}));
+app.options(cors({origin:true,credentials:true}));
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  next();
+});
+
+var port = process.env.PORT || 3003;
+app.set('port', port);
+
+app.locals.name = 'ProtectMyChild';
+
+process.on("uncaughtException", function (err) {
+  console.log({data: 'uncaughtException', error: err.stack});
+});
+
+http.listen(app.get('port'), function () {
+  console.log(app.locals.name + ' server running...');
+  console.log('Port: ' , app.get('port'));
+  console.log('Mode: ' , process.env.NODE_ENV?'development':'production');
+  console.log(new Date());
+});
+
+app.get('/', function (req, res, next) {
+    req.sendFile({root:process.cwd()+'/public/index.html'});
+});
+
+app.get('/privacy',function(req, res, next){
+  res.sendFile('privacypolicy.html', { root: path.join(__dirname, '../public') });
+});
+
+app.get('/*', function (req, res) {
+  res.send(app.locals.name);
+});
+
+process.on('SIGTERM', function () {
+    process.exit(0);
+});
---
+++ index.js
@@ -0,0 +1,31 @@
+var exec = require('child_process').exec;
+var cluster = require('cluster');
+var isProd = process.env.NODE_ENV == 'production';
+var numCPUs = isProd ? 1 : 1; // require('os').cpus().length
+//var workers = {};
+//var clusters = {};
+
+function startServer() {
+    require('./core/server');
+}
+
+if (cluster.isMaster && isProd) { // cluster.isWorker
+    for (var i = 1; i <= numCPUs; i++) {
+        var workerInfo = {worker_id: i};
+        cluster.fork(workerInfo);
+    }
+} else if (isProd){
+    console.log('cluster id:',cluster.worker.id,'process id:',process.pid);
+    startServer(cluster.worker.id);
+} else {
+    startServer();
+}
+
+//cluster.workers[id].process.pid
+cluster.on('exit', function (worker) {
+    console.log('cluster ' + worker.id + ' died :(');
+    console.log('Starting a new cluster...');
+    var workerInfo = worker.process.workerInfo;
+    var newWorker = cluster.fork(workerInfo);
+    newWorker.process.workerInfo = workerInfo;
+});
