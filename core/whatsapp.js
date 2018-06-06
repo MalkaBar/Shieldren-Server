a360238cd17a4c16dc50b3a26b5531c37a9d051a
Omer Avrech <omera@ceragon.com>
2018-06-06 11:35:50 +0300

    update json parsing

--- core/whatsapp.js
+++ core/whatsapp.js
@@ -14,54 +14,60 @@ module.exports = class WhatsApp {
                 if (debugMode) errorMessage(err);
                 this.socket.emit('qrError', 'Error received. close session. [err = ' + err.message + ']');
             } else {
-                let buffer = "";
+                var buffer = "";
                 this.childInfo = result;
                 this.subproccess = spawn(Script.executer, [Script.path, this.childInfo.phoneNumber], { detached: true });
-                this.subproccess.stdout.on('data', (data) => { dataReceived(data.toString()); }); 
+                this.subproccess.stdout.on('data', (data) => { 
+                    try {
+                        let json = JSON.parse(data.toString());
+                        dataReceived(json);
+                        buffer = "";
+                    } catch {
+                        try {
+                            let json = JSON.parse(buffer + data.toString());
+                            dataReceived(json);
+                            buffer = "";
+                        } catch {
+                            buffer += data.toString();
+                        }
+                    }
+                }); 
                 this.subproccess.stderr.on('data', (data) => { if (debugMode) errorMessage('Error: ' + data); });
                 this.subproccess.on('exit', (data) => { errorMessage('Close connection for ' + this.uniuqeID); });
             }
         });    
     }
 
-    dataReceived(message) {
-        try {
-            if (debugMode) debugMessage("Json arrived " + message)
-            let obj = JSON.parse(message);
-            switch (obj.code) {
-                case -1:
-                    this.subproccess.kill('SIGTERM');
-                case 0:             //Whatsapp session been logout
-                    algoController.qrBeenClosed(this.childInfo.childid);
-                    break;
-                case 1:             //Send QR to user
-                    this.qrString += obj.data.chunk;
-                    if (obj.data.index === -1) {
-                        this.socket.emit('qrArrived', this.qrString);
-                    }
-                    break;
-                case 2:             //Notify user about successful scan
-                    algoController.qrBeenScanned(this.childInfo.childid, (err) => {
-                        if (err) { return this.socket.emit('qrError', 'Error while update DB.'); }
-                        this.socket.emit('qrScanned','QR been scanned. closing session.');
-                        this.socket.disconnect(true);
-                    });                    
-                    break;
-                case 3:             //New message received
-                if (debugMode) debugMessage("Message been sent to classifier: " + JSON.stringify(data))
-                    algoController.sentenceRecieved(obj.data);
-                    break;
-                case 5:             //Child process start to run
-                    normalMessage('Start Whatsapp session [' + obj.seesionID + ']')
-                    this.uniuqeID = obj.seesionID;
-                    break;
-                default:
-                    if (debugMode) debugMessage('Default: ' + message);
-            }
-        }
-        catch (err) {
-            errorMessage(err);
-            this.socket.emit('qrError', 'Error received. close session. [err = ' + err.message + ']');
+    dataReceived(json) {
+        switch (json.code) {
+            case -1:
+                this.subproccess.kill('SIGTERM');
+            case 0:             //Whatsapp session been logout
+                algoController.qrBeenClosed(this.childInfo.childid);
+                break;
+            case 1:             //Send QR to user
+                this.qrString += json.data.chunk;
+                if (json.data.index === -1) {
+                    this.socket.emit('qrArrived', this.qrString);
+                }
+                break;
+            case 2:             //Notify user about successful scan
+                algoController.qrBeenScanned(this.childInfo.childid, (err) => {
+                    if (err) { return this.socket.emit('qrError', 'Error while update DB.'); }
+                    this.socket.emit('qrScanned','QR been scanned. closing session.');
+                    this.socket.disconnect(true);
+                });                    
+                break;
+            case 3:             //New message received
+            if (debugMode) debugMessage("Message been sent to classifier: " + JSON.stringify(data))
+                algoController.sentenceRecieved(json.data);
+                break;
+            case 5:             //Child process start to run
+                normalMessage('Start Whatsapp session [' + json.seesionID + ']')
+                this.uniuqeID = json.seesionID;
+                break;
+            default:
+                if (debugMode) debugMessage('Default: ' + message);
         }
     }
 };
