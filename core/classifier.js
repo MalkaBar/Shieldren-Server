e013a8c657dc41ff5562c521ed67ab1120ec03f3
Omer Avrech <omera@ceragon.com>
2018-08-22 11:33:30 +0300

    bug fix

--- core/classifier.js
+++ core/classifier.js
@@ -11,30 +11,29 @@ class Classifier {
     constructor() {
         this.subproccess = spawn(Algorithm.executer, [Algorithm.path], { stdio: 'pipe'});
         this.subproccess.stdout.on("data", function (data) { processRecievedData(data.toString()); });
-        this.subproccess.stderr.on("data", function (data) { if (debugMode) console.log("[\x1b[34mCLASSIFIER\x1b[0m] " + data.toString()); });
-        this.subproccess.on("exit", function (data) { console.log("[\x1b[34mCLASSIFIER\x1b[0m] been exited. Reason: " + data); });
+        this.subproccess.stderr.on("data", function (data) { debugMessage(data.toString()); });
+        this.subproccess.on("exit", function (data) { errorMessage("been exited. Reason: " + data); });
     }
 
     clasiffy(sentenceData) {
         let identifier = uniqid();
         ClasiffierSentences[identifier] = sentenceData;
         this.subproccess.stdin.write('{"id": "' + identifier + '", "sentence": "' + sentenceData.message.toString() + '"}\n');
-        if (debugMode) console.log('[\x1b[34mCLASSIFIER\x1b[0m] Sentence send to clasification: ' + sentenceData.message);
+        debugMessage("Sentence send to clasification: " + sentenceData.message);
     }
 }
 
 function processRecievedData(data)
 {
     try {
-        if (debugMode) console.log('[\x1b[34mCLASSIFIER\x1b[0m] JSON ARRIVED:' + data);
+        debugMessage("Received Data = " + data);
         let obj = JSON.parse(data);
         switch (obj.code) {
             case 1:             // Model have loaded and calssifier ready to start
-                console.log('[\x1b[34mCLASSIFIER\x1b[0m] Finish loading.');
+                normalMessage("Finish loading.");
                 break;
             case 2:             // Classify sentence been recieved
-                if (debugMode) console.log(JSON.stringify(data));
-                   
+                debugMessage(JSON.stringify(data));
                 let sentenceData = ClasiffierSentences[obj.data.identifier];
                 delete ClasiffierSentences[obj.data.identifier];
                 sentenceData.group = sentenceData.group?1:0;
@@ -43,8 +42,10 @@ function processRecievedData(data)
                 {
                     case 1:
                         db.run("INSERT INTO [shieldren].[messages] VALUES ('" + sentenceData.caller + "', '" + sentenceData.callee + "', '" + sentenceData.timestamp + "', '" + sentenceData.group + "', '" + sentenceData.message + "', '" + obj.data.classification + "');",
-                            (err) => { console.log("[\x1b[34mCLASSIFIER\x1b[0m] Error:" + err);                           }
-                        );
+                        (err, data) => {
+                            if (err) errorMessage(err); 
+                            else debugMessage(data);
+                        });
                         /*
                         notification.Notice("הורה יקר, נמצא כי ילדך חווה כעת בריונות רשת.", sentenceData.parentEmail)
                             .then((data) => { console.log("[Urban] " + data); })
@@ -58,7 +59,10 @@ function processRecievedData(data)
                         break;
                     case -1:
                         db.run("INSERT INTO [shieldren].[messages] VALUES ('" + sentenceData.caller + "', '" + sentenceData.callee + "', '" + sentenceData.timestamp + "', '" + sentenceData.group + "', '" + sentenceData.message + "', '" + obj.data.classification + "');",
-                            (err) => { console.log("[\x1b[34mCLASSIFIER\x1b[0m] Error:" + err);                           }
+                            (err, data) => {
+                                if (err) errorMessage(err); 
+                                else debugMessage(data);
+                            }
                         );
                         break;
                     default:
@@ -66,15 +70,19 @@ function processRecievedData(data)
                 }
                 break;
             case 3:             // Script start to run. Loading the model.
-                console.log('[\x1b[34mCLASSIFIER\x1b[0m] Start loading');
+                normalMessage("Start loading");
                 break;
             default:            // Errors
                 throw new Error(data);
         }
     }
     catch(err) {
-        console.log('[\x1b[34mCLASSIFIER\x1b[0m] Error: ' + err);
+        errorMessage(err);
     }
 }
+
+function debugMessage(message)  { if (debugMode) console.log("[\x1b[34mCLASSIFIER\x1b[0m] \x1b[36mDEBUG\x1b[0m: " + message); }
+function errorMessage(message)  { console.log("[\x1b[34mCLASSIFIER\x1b[0m] \x1b[36mERROR\x1b[0m: " + message); }
+function normalMessage(message) { console.log("[\x1b[34mCLASSIFIER\x1b[0m] " + message); }
 
 module.exports = new Classifier();
